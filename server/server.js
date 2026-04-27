a1b7cb19f3692102eeaa21f7530e149d5216d769
loopvanta <meshram02005komal@gmail.com>
2026-04-27 14:22:16 +0530

    port fix for render

--- server/server.js
+++ server/server.js
@@ -30,6 +30,27 @@
 
 // app.listen(5000, ()=>console.log("Server running 🚀"));
 
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// const registerRoute = require("./routes/register");
+// app.use("/register", registerRoute);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT} 🚀`);
+// });
+
+// const adminRoutes = require("./routes/admin");
+// app.use("/admin", adminRoutes);
+
+
 const express = require("express");
 const cors = require("cors");
 
@@ -38,12 +59,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
 const registerRoute = require("./routes/register");
 app.use("/register", registerRoute);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000 🚀");
-});
-
 const adminRoutes = require("./routes/admin");
 app.use("/admin", adminRoutes);
+
+// PORT FIX
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
